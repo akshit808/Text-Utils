695b21482d86fe6ecc89026f11d6e5742c52ebbf
akshit808 <vijayakshit20@gmail.com>
2021-05-20 13:17:59 +0530

    first commit

---
+++ textutils/views.py
@@ -0,0 +1,65 @@
+# I have created this file
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request,'index.html',{'akshit':24})
+
+def analyze(request):
+ data = request.POST.get('text', 'No text entered')
+
+ remPunc = request.POST.get('punc', 'of')
+ caps = request.POST.get('caps', 'of')
+ newLineRem = request.POST.get('newLineRem', 'of')
+ spaceRem = request.POST.get('spaceRem', 'of')
+
+ charcount = request.POST.get('charcount', 'of')
+ strr = data
+ purpose = ""
+
+ if remPunc == 'on':
+  tempStr = ""
+  puns = '''!@#$%^&*();'.,/:?>'''
+  for i in data:
+   if i not in puns:
+    tempStr = tempStr + i
+  params = {'purpose':'remove Punctuations' , 'answer':tempStr}
+  strr = tempStr
+  purpose += " | Remove Punctuations "
+  # return render(request, 'analyze.html', params)
+ if caps == 'on':
+  print("2",strr)
+  strr = strr.upper()
+  params = {'purpose':'Caps' , 'answer':strr}
+  purpose += "| Caps |"
+  # return render(request, 'analyze.html', params)
+
+ if newLineRem == 'on':
+  tempStr=""
+  for i in strr:
+   if i != '\n' and i!="\r":
+    tempStr += i
+  params = {'purpose':'New Line remove' , 'answer':tempStr}
+  strr = tempStr
+  purpose += "| remove new line "
+  # return render(request, 'analyze.html', params)
+
+ if spaceRem == 'on':
+  tempStr = ""
+  for index, ch in enumerate(strr):
+   if not (strr[index] == " " and strr[index+1]==" "):
+    tempStr += ch
+  params = {'purpose':'spaces remove' , 'answer':tempStr}
+  strr = tempStr
+  purpose += "| Spaces remove |"
+
+ params = {'purpose':purpose , 'answer':strr}
+
+ if remPunc == 'on' or caps == 'on' or newLineRem == 'on' or spaceRem == 'on':
+  return render(request, 'analyze.html', params)
+ else:
+  return HttpResponse('error')
+
+
+
